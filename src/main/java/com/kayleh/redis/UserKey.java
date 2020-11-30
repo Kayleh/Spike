682890e55216be7bf65c79bbcb5b37115e136c06
Kayleh <yao924934908@yeah.net>
2020-11-30 09:05:20 +0800

    1

--- src/main/java/com/kayleh/redis/UserKey.java
+++ src/main/java/com/kayleh/redis/UserKey.java
@@ -2,7 +2,7 @@ package com.kayleh.redis;
 
 /**
  * 展现出抽象类的实现类
- *
+ * <p>
  * 它的构造函数就是用的抽象父类中的构造函数，而且定义了两个静态字段，
  * 一种是根据Id来生成前缀，前缀格式会根据getPrefix()方法，表示为类名+：+id
  *
@@ -19,5 +19,4 @@ public class UserKey extends BasePrefix
     //UserKey的两种前缀形式,一种是根据id另一种根据name
     public static UserKey getById = new UserKey("id");
     public static UserKey getByName = new UserKey("name");
-
 }
